c3d2b1082872ebee0e2dc55febd2919adc7eafbf
MarkAv777 <64240132+MarkAv777@users.noreply.github.com>
2020-04-24 17:10:49 -0400

    Update ProbaGit.java

--- src/TestGit/ProbaGit.java
+++ src/TestGit/ProbaGit.java
@@ -3,7 +3,10 @@ package TestGit;
 public class ProbaGit {
     public static void main(String[] args) {
         System.out.println("Task proba");
-
+        System.out.println("Task proba");
+        System.out.println("Task proba");
+        
+        
 
         // tytyrtryrtyry
 
